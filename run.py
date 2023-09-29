cf7d06450a08cdae3f5d698f64eb388480aa6901
JosephOConnell <>
2023-09-29 17:14:40 +0000

    Import random and added functions

---
+++ run.py
@@ -0,0 +1,22 @@
+import random
+
+def random_maths_quiz():
+    def addition():
+    
+
+    def subtraction():
+
+
+    def addition_and_subtraction():
+
+
+def higher_lower():
+
+
+def play_again():
+
+
+def main():
+
+
+main():
